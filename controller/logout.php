e362dffcc045cd0f7f64e4112488c57ab9a70f27
Ashvini Kumar <S1E-@users.noreply.github.com>
2014-12-01 09:57:53 +0530

    Update logout.php

--- controller/logout.php
+++ controller/logout.php
@@ -1,13 +1,4 @@
 <?php
-/********************
- * logout.php
- *
- * CSCI S-75
- * Project 1
- * Chris Gerber
- *
- * Logout controller
- ********************/
 
 require_once('../includes/helper.php');
 
