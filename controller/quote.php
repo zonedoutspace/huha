c1091e8dde3267e59cfc9813e0f37561e11a1bae
Ashvini Kumar <S1E-@users.noreply.github.com>
2014-12-01 09:58:30 +0530

    Update quote.php

--- controller/quote.php
+++ controller/quote.php
@@ -1,13 +1,4 @@
 <?php
-/*******************
- * quote.php
- *
- * CSCI S-75
- * Project 1
- * Chris Gerber
- *
- * Quote controller
- *******************/
 
 require_once('../model/model.php');
 require_once('../includes/helper.php');
